b7c2e7a577ad6284f51054bf886e1c9ea2cf3169
Emilien Baudet <emilien.baudet@gmail.com>
2015-01-18 20:11:47 +0100

    monitorqt avancement

--- MonitorQt.hpp
+++ MonitorQt.hpp
@@ -14,12 +14,21 @@
 # define MONITORQT_HPP
 
 #include "IMonitorDisplay.hpp"
+#include <QApplication>
+#include "Qtwindow.h"
 #include <iostream>
+#include <vector>
 
 class MonitorQt : public IMonitorDisplay
 {
+private:
+    QApplication        *_qa;
+    QtWindow            *_qwindow;
+    std::vector<QWidget *>   _qwidget;
+    MonitorQt(void);
+
 public:
-	MonitorQt(void);
+    MonitorQt(int, char **);
 	MonitorQt(MonitorQt const &);
 	MonitorQt& operator=(MonitorQt const &);
 	~MonitorQt(void);
@@ -27,6 +36,11 @@ public:
 	// member functions
 	void	init();
 	void	quit();
+    int     addWidget(void);
+
+    // getters & setters
+    QApplication    *getQA(void) const;
+    QtWindow        *getQWindow(void) const;
 };
 
 
