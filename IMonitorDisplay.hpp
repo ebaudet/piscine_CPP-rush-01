c8db352e74ea7a8312d03a109a831d79b2c5b385
gmp89 <gm.petrov@yahoo.fr>
2015-01-18 18:01:12 +0100

    update

--- IMonitorDisplay.hpp
+++ IMonitorDisplay.hpp
@@ -4,6 +4,7 @@
 class IMonitorDisplay{
     public:
         virtual void init() = 0;
+        virtual void quit() = 0;
         virtual ~IMonitorDisplay() = 0;
 };
 
--- MonitorNcurses.hpp
+++ MonitorNcurses.hpp
@@ -1,6 +1,9 @@
 #ifndef MONITORNCURSES_H
 # define MONITORNCURSES_H
 
+# include <ncurses.h>
+# include <iostream>
+# include <string>
 # include "IMonitorDisplay.hpp"
 
 class MonitorNcurses : public IMonitorDisplay{
@@ -10,6 +13,7 @@ class MonitorNcurses : public IMonitorDisplay{
         virtual ~MonitorNcurses();
         MonitorNcurses & operator=(MonitorNcurses const & rhs);
         virtual void    init();
+        virtual void quit();
 };
 
 #endif // MONITORNCURSES_HPP
